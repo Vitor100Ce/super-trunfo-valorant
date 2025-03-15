b3ff385462cf80c1e2f19b3dae2a7dc03b10af62
Vitor Ramos <vitor95ramos@gmail.com>
2025-03-15 12:07:56 -0300

    feat: criada a rota jogar-sem-conta

--- app/Routes/web.php
+++ app/Routes/web.php
@@ -2,6 +2,7 @@
 
 use Pecee\SimpleRouter\SimpleRouter;
 use app\Controllers\HomeController;
+use app\Controllers\JogarSemContaController;
 
-// Rota para a página inicial
 SimpleRouter::get('/', [HomeController::class, 'index']);
+SimpleRouter::get('/jogar-sem-conta', [JogarSemContaController::class, 'jogarSemConta']);
