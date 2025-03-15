3ec63b4a11bf926a71d6bf6aaa129e073d2b9ee4
Vitor Ramos <vitor95ramos@gmail.com>
2025-03-15 15:38:58 -0300

    feat: cria a rota get-cartas. Essa rota mostrar as cartas disponíveis no banco de dados

--- app/Routes/web.php
+++ app/Routes/web.php
@@ -3,6 +3,9 @@
 use Pecee\SimpleRouter\SimpleRouter;
 use app\Controllers\HomeController;
 use app\Controllers\JogarSemContaController;
+use app\Controllers\BaralhoController;
 
 SimpleRouter::get('/', [HomeController::class, 'index']);
 SimpleRouter::get('/jogar-sem-conta', [JogarSemContaController::class, 'jogarSemConta']);
+SimpleRouter::get('/get-cartas', [BaralhoController::class, 'getCartas']);
+
