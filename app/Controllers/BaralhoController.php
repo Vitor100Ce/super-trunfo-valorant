8454a84b4705084592e9a9d5638a2b6c89046130
Vitor Ramos <vitor95ramos@gmail.com>
2025-03-19 20:58:29 -0300

    chore: retira as carta que estavam mocada e foi substituído por um método que faz select no banco de dados

--- app/Controllers/BaralhoController.php
+++ app/Controllers/BaralhoController.php
@@ -2,86 +2,24 @@
 
 namespace app\Controllers;
 use app\Core\View;
+use app\Models\BaralhoModel;
+
 
 class BaralhoController{
 
     public function getCartas(){
 
-        $cartas = [
-
-            [ 
-                'id'=> 1, 
-                'agente' => 'Brimstone', 
-                'img' => '\images\brimstone.png', 
-                'dano' => 10,
-                'velocidade' => 10,
-                'tecnologia' => 15,
-                'radianita' => 8
-            ],
-            [ 
-                'id'=> 2, 
-                'agente' => 'Cypher', 
-                'img' => '\images\cypher.png', 
-                'dano' => 10,
-                'velocidade' => 10,
-                'tecnologia' => 15,
-                'radianita' => 8
-            ],
-            [ 
-                'id'=> 2, 
-                'agente' => 'Killjoy', 
-                'img' => '\images\killjoy.png', 
-                'dano' => 10,
-                'velocidade' => 10,
-                'tecnologia' => 15,
-                'radianita' => 8
-            ],
-            [ 
-                'id'=> 2, 
-                'agente' => 'Phoenix', 
-                'img' => '\images\phoenix.png', 
-                'dano' => 10,
-                'velocidade' => 10,
-                'tecnologia' => 15,
-                'radianita' => 8
-            ],
-            [ 
-                'id'=> 2, 
-                'agente' => 'Sage', 
-                'img' => '\images\sage.png', 
-                'dano' => 10,
-                'velocidade' => 10,
-                'tecnologia' => 15,
-                'radianita' => 8
-            ],
-            [ 
-                'id'=> 2, 
-                'agente' => 'Sova', 
-                'img' => '\images\sova.png', 
-                'dano' => 10,
-                'velocidade' => 10,
-                'tecnologia' => 15,
-                'radianita' => 8
-            ],
-            [ 
-                'id'=> 2, 
-                'agente' => 'Viper', 
-                'img' => '\images\viper.png', 
-                'dano' => 10,
-                'velocidade' => 10,
-                'tecnologia' => 15,
-                'radianita' => 8
-            ]
-        ];
-
+        $cartas = (new BaralhoModel)->getCartas();
+       
         return View::render('montar-baralho.twig', ['cartas' => $cartas]);
 
     }
 
-    public function montarBaralho(){
+    public function montarBaralhoSemConta(){
+
+        $idCartas = $_POST['selectedCards'] ?? [];
 
         $baralho = [];
-
          
     }
 }
