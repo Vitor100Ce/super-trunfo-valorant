81a720efa60ff5c7fa114ff7747c08ab00db16fd
Vitor Ramos <vitor95ramos@gmail.com>
2025-03-19 23:44:39 -0300

    chore: adicionei o bloco try catch no metódo setBaralhoSemConta

--- app/Controllers/BaralhoController.php
+++ app/Controllers/BaralhoController.php
@@ -4,8 +4,7 @@ namespace app\Controllers;
 use app\Core\View;
 use app\Models\BaralhoModel;
 
-
-class BaralhoController{
+class BaralhoController {
 
     public function getCartas(){
 
@@ -15,11 +14,30 @@ class BaralhoController{
 
     }
 
-    public function montarBaralhoSemConta(){
+    public function setBaralhoSemConta(){
 
-        $idCartas = $_POST['selectedCards'] ?? [];
+        try{
 
-        $baralho = [];
-         
+            session_start();
+
+            unset($_SESSION['baralho']);
+
+            if(!isset($_POST['selectedCards'])){
+                throw new Exception('Nenhuma carta foi selecionada');
+            }
+
+            $idCartas = $_POST['selectedCards'] ?? [];
+
+            $baralho = (new BaralhoModel)->getBaralho($idCartas);
+
+            $_SESSION['baralho'] = $baralho;
+
+            return true;
+
+        }catch (Exception $e){
+            erro_log("Erro ao montar baralho: ".$e->getMessage());
+            return false;
+        }
+
     }
 }
